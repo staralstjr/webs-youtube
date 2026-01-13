579dcea66ed33b9ff3598b62ccc4da6043fe6725
MinseokKwon <staralstjr@naver.com>
2026-01-13 13:53:24 +0900

    섹션 컴퍼넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -12,24 +12,31 @@ import Channel from './pages/Channel';
 import Video from './pages/Video';
 import Search from './pages/Search';
 import Not from './pages/Not';
+import Header from './components/section/Header';
+import Footer from './components/section/Footer';
+import Main from './components/section/Main';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/today" element={<Today />} />
-        <Route path="/developer" element={<Developer />} />
-        <Route path="/webd" element={<Webd />} />
-        <Route path="/website" element={<Website />} />
-        <Route path="/gsap" element={<Gsap />} />
-        <Route path="/port" element={<Port />} />
-        <Route path="/youtube" element={<Youtube />} />
-        <Route path="/channel/:channelId" element={<Channel />} />
-        <Route path="/video/:videoId" element={<Video />} />
-        <Route path="/search/:searchId" element={<Search />} />
-        <Route path="/*" element={<Not />} />
-      </Routes>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/webd" element={<Webd />} />
+          <Route path="/website" element={<Website />} />
+          <Route path="/gsap" element={<Gsap />} />
+          <Route path="/port" element={<Port />} />
+          <Route path="/youtube" element={<Youtube />} />
+          <Route path="/channel/:channelId" element={<Channel />} />
+          <Route path="/video/:videoId" element={<Video />} />
+          <Route path="/search/:searchId" element={<Search />} />
+          <Route path="/*" element={<Not />} />
+        </Routes>
+      </Main>
+      <Footer />
     </BrowserRouter>
   );
 };
---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer id="footer" role="contentsinfo">
+      Footer
+    </footer>
+  );
+};
+
+export default Footer;
