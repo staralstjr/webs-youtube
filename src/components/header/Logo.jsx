fd41b54b84a6afae41d13e8222af48637fb8059c
MinseokKwon <staralstjr@naver.com>
2026-01-13 16:41:56 +0900

    헤더 영역 데이터 작업

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import msIcon from '../../assets/img/minseokIcon.png';
+const Logo = () => {
+  return (
+    <h1 className="header__logo">
+      <h1 className="header__logo">
+        <a href="/">
+          <img src={msIcon} aria-hidden="true"></img>
+          <span>
+            webs
+            <br />
+            youtube
+          </span>
+        </a>
+      </h1>
+    </h1>
+  );
+};
+
+export default Logo;
