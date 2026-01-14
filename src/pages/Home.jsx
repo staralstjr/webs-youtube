5e40944da0a3da5454a6df95095cd5dc2a25b380
MinseokKwon <staralstjr@naver.com>
2026-01-14 16:13:51 +0900

    비디오 컴포넌트 통합 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,12 @@ import Website from '../components/contents/Website';
 import Gsap from '../components/contents/Gsap';
 import Portfolio from '../components/contents/Portfolio';
 import Youtube from '../components/contents/Youtube';
+import VideoSlider from '../components/videos/VideoSlider';
+import { webdText } from '../data/webd';
+import { websiteText } from '../data/website';
+import { gsapText } from '../data/gsap';
+import { portfolioText } from '../data/porfolio';
+import { youtubeText } from '../data/youtube';
 
 const Home = () => {
   return (
@@ -16,11 +22,31 @@ const Home = () => {
     >
       <Today />
       <Developer />
-      <Webd />
-      <Website />
-      <Gsap />
-      <Portfolio />
-      <Youtube />
+      <VideoSlider
+        videos={webdText}
+        title="😮 웹디자인기능사 준비는 이걸로!"
+        id="webd"
+      />
+      <VideoSlider
+        videos={websiteText}
+        title="😛 웹표준 사이트 만들기 기초 다지기"
+        id="website"
+      />
+      <VideoSlider
+        videos={gsapText}
+        title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!"
+        id="gsap"
+      />
+      <VideoSlider
+        videos={portfolioText}
+        title="🤗 포트폴리오 만드는 방법을 공유합니다."
+        id="portfolio"
+      />
+      <VideoSlider
+        videos={youtubeText}
+        title="😱 지금 이 코딩을 영상으로"
+        id="youtube"
+      />
     </Main>
   );
 };
