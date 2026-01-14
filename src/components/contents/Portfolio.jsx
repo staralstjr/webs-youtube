d51121de8884ad86293b36c2e5e209998e91f5dc
MinseokKwon <staralstjr@naver.com>
2026-01-14 15:44:18 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/components/contents/Portfolio.jsx
+++ src/components/contents/Portfolio.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { portfolioText } from '../../data/porfolio';
+import { Link } from 'react-router-dom';
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section id="portfolio">
+      <h2>🤓GSAP패럴랙스 효과를 하고 싶다면!</h2>
+      <div className="video__inner">
+        {portfolioText.map((video, key) => (
+          <div className="video" key={key}>
+            <div className="video__thumb play__icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/contents/Youtube.jsx
+++ src/components/contents/Youtube.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { youtubeText } from '../../data/youtube';
+import { Link } from 'react-router-dom';
 
 const Youtube = () => {
-  return <div>Youtube</div>;
+  return (
+    <section id="youtube">
+      <h2>😛 웹표준 사이트 만들기 기초 다지기</h2>
+      <div className="video__inner">
+        {youtubeText.map((video, key) => (
+          <div className="video" key={key}>
+            <div className="video__thumb play__icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Youtube;
