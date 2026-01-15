848d85f5807e5b79b412854fb036e432ac507ffa
MinseokKwon <staralstjr@naver.com>
2026-01-15 13:12:14 +0900

    Rapid API 및 더보기 설정하기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,28 +2,43 @@ import React, { useEffect, useState } from 'react';
 import Main from '../components/section/Main';
 import { useParams } from 'react-router-dom';
 import VideoSearch from '../components/videos/VideoSearch';
+import { fetchFromAPI } from '../utils/api';
 
 const Search = () => {
   const { searchId } = useParams();
   const [videos, setVideos] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState(null);
 
   useEffect(() => {
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        setVideos(result.items);
-      })
-      .catch((error) => console.log(error));
+    setVideos([]);
+    fetchVideos(searchId);
   }, [searchId]);
 
+  const fetchVideos = (query, pageToken = '') => {
+    fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+      .then((data) => {
+        setNextPageToken(data.nextPageToken);
+        setVideos((prevVideos) => [...prevVideos, ...data.items]);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
+  const handleLoadMore = () => {
+    if (nextPageToken) {
+      fetchVideos(searchId, nextPageToken);
+    }
+  };
+
   return (
-    <Main title="유투브 검색" description="유튜브 검색 결과 페이지입니다.">
+    <Main title="유튜브 검색" description="유튜브 검색 결과 페이지입니다.">
       <section id="searchPage">
         <div className="video__inner search">
           <VideoSearch videos={videos} />
+        </div>
+        <div className="video__more">
+          {nextPageToken && <button onClick={handleLoadMore}>더 보기</button>}
         </div>
       </section>
     </Main>
